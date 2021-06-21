40ee1fa7ea1481fba7c82431ee879a42c5f417a9
sandexp <XieYunfei0921@126.com>
2021-06-21 23:58:25 +0800

    rename AbstractRecord to Row

--- src/main/java/com/github/lolidb/storage/Row.java
+++ src/main/java/com/github/lolidb/storage/Row.java
@@ -2,13 +2,18 @@ package com.github.lolidb.storage;
 
 import com.github.lolidb.storage.tree.value.StructValue;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channel;
+import java.nio.channels.FileChannel;
 
 /**
  * Abstract record can mean fixed-length record or unfixed-length.
  * And both can persist into disk and have their unique storage-head format.
  */
-public class AbstractRecord implements Serializable {
+public class Row implements Serializable {
 
 	/**
 	 * The first address of record. can be located by it.
@@ -64,12 +69,12 @@ public class AbstractRecord implements Serializable {
 		return data.getSize();
 	}
 
-	public void writeObject(){
-		
+	public void writeObject(ByteBuffer buffer, FileChannel channel) throws IOException {
+		data.writeObject(buffer,channel);
 	}
 
-	public StructValue readObject(StructValue schema){
-
-		return null;
+	public StructValue readObject(ByteBuffer buffer,int offset) throws IOException {
+		data.readObject(buffer,offset);
+		return data;
 	}
 }
