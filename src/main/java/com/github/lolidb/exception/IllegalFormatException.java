7cec68fb7f093e957e5be4156c6d66356d95f960
sandexp <XieYunfei0921@126.com>
2021-06-30 16:23:17 +0800

    add new exception

---
+++ src/main/java/com/github/lolidb/exception/IllegalFormatException.java
@@ -0,0 +1,8 @@
+package com.github.lolidb.exception;
+
+public class IllegalFormatException extends IllegalArgumentException {
+
+	public IllegalFormatException(String format) {
+		super(format);
+	}
+}
